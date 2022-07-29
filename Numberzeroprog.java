51fc1ab4862a51b8e57ff5244549d1b39f221240
ekta <aektapatel0102@gmail.com>
2022-07-29 15:04:43 +0100

    numberzero learning java prg

---
+++ Numberzeroprog.java
@@ -0,0 +1,16 @@
+package Numberzero4;
+
+public class Numberzeroprog {
+    public static void main(String[] args) {
+        int n =0;
+        if(n>0) {
+            System.out.println("number is positive");
+        }
+        else if(n<0) {
+            System.out.println("number is negative");
+        }else
+        {
+            System.out.println("number is zero");
+        }
+    }
+}
